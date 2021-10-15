3860eaec7b019ea1b8133f3c4eefd5f1787f15a5
ewileyrev <ethan.wiley@revature.net>
2021-10-15 08:52:08 -0400

    fix

--- AceChat/src/main/java/com/acechat/AceChatApplication.java
+++ AceChat/src/main/java/com/acechat/AceChatApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AceChatApplication {
-	
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-         return application.sources(AceChatApplication.class);
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AceChatApplication.class, args);
