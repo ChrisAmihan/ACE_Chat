e1cef52db5f4e8900df7a9d116393cc16b1ca1ed
ewileyrev <ethan.wiley@revature.net>
2021-10-14 20:08:34 -0400

    just making sure everything is updated

--- AceChat/src/main/java/com/acechat/AceChatApplication.java
+++ AceChat/src/main/java/com/acechat/AceChatApplication.java
@@ -2,9 +2,14 @@ package com.acechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AceChatApplication {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(AceChatApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AceChatApplication.class, args);
