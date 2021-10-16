96e1a775e8e0f56afd1170f64142684b3bf88595
ewileyrev <ethan.wiley@revature.net>
2021-10-16 09:59:03 -0400

    pom

--- AceChat/src/main/java/com/acechat/AceChatApplication.java
+++ AceChat/src/main/java/com/acechat/AceChatApplication.java
@@ -3,6 +3,9 @@ package com.acechat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AceChatApplication {
@@ -11,5 +14,18 @@ public class AceChatApplication {
 		SpringApplication.run(AceChatApplication.class, args);
 		
 	}
+	
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //allows C O R s mapping
+            	 registry.addMapping("/**")
+                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 
+            }
+        };
+    }
 }
