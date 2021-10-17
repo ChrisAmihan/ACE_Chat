533f274257da2c6be165d8f7a2e4b7154c1d4c2b
ewileyrev <ethan.wiley@revature.net>
2021-10-17 14:41:50 -0400

    8088

--- AceChat/src/main/java/com/acechat/AceChatApplication.java
+++ AceChat/src/main/java/com/acechat/AceChatApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.acechat.repository")
 public class AceChatApplication {
 
 	public static void main(String[] args) {
