a1379437b120aaef6feb6465721b94602ddc5088
ewileyrev <ethan.wiley@revature.net>
2021-10-17 15:20:32 -0400

    8088

--- AceChat/src/main/java/com/acechat/repository/BlockRepository.java
+++ AceChat/src/main/java/com/acechat/repository/BlockRepository.java
@@ -3,12 +3,13 @@ package com.acechat.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import com.acechat.model.Block;
 import com.acechat.model.User;
 
-@Repository
+
+@Component
 public interface BlockRepository extends JpaRepository<Block,Integer>{
 	public <S extends Block> S save(S block);
 
